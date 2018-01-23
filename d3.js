6c643c7463054be6ec8d5426c53e7095fc770103
Rufina Tsoi <rufina0524@gmail.com>
2018-01-23 22:55:36 +0000

    Add drag listener.

--- d3.js
+++ d3.js
@@ -1,85 +1,97 @@
 
 // Transform SVG project from vertical to horizontal.
 const diagonal = d3.svg.diagonal()
-									.projection(d => [d.y, d.x]);
+                  .projection(d => [d.y, d.x]);
 
 // Zoom function.
 const zoom = (element) => {
-		element.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    element.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 };
 
 const treeJson = d3.json('data.json', (error, treeData) => {
 
-	// Zoom listener callback.
-	const zoomListener = d3.behavior
-												.zoom()
-												.scaleExtent([0.1, 3])
-												.on("zoom", () => {
-													zoom(svgGroup);
-												});
+  // Zoom listener callback.
+  const zoomListener = d3.behavior.zoom()
+                        .scaleExtent([0.1, 3])
+                        .on("zoom", () => {
+                          zoom(svgGroup);
+                        });
 
-	// Create SVG Canvas.
-	const baseSvg = d3.select('#tree-container')
-										.append('svg')
-										.attr('width', $(document).width())
-										.attr('height', $(document).height())
-										.call(zoomListener);
+  // Drag listener callback.
+  const dragListener = d3.behavior.drag()
+                        .on("dragstart", () => {
+                          console.info("[drag start]")
+                        })
+                        .on("drag", () => {
+                          console.info("[drag]")
+                        })
+                        .on("dragend", () => {
+                          console.info("[drag end]");
+                        });
 
-	// Create an SVG group which can be zoomed.
-	const svgGroup = baseSvg.append('g');
+  // Create SVG Canvas.
+  const baseSvg = d3.select('#tree-container')
+                    .append('svg')
+                    .attr('width', $(document).width())
+                    .attr('height', $(document).height())
+                    .call(zoomListener);
 
-	const tree = d3.layout.tree()
-								.size([$(document).height(), $(document).width() / 2]);
+  // Create an SVG group which can be zoomed.
+  const svgGroup = baseSvg.append('g');
 
-	try {
+  const tree = d3.layout.tree()
+                .size([$(document).height(), $(document).width() / 2]);
 
-	const nodes = tree.nodes(treeData).reverse();
-	const links = tree.links(nodes);
-	let translateX = $(document).width() * 0.25;
-	let translateY = $(document).height() * 0.5;
-	let i = 0;
+  try {
 
-	const node = svgGroup.selectAll('g.nodes')
-								.data(nodes, (item) => item.id || (item.id = i++));
+  const nodes = tree.nodes(treeData).reverse();
+  const links = tree.links(nodes);
+  let translateX = $(document).width() * 0.25;
+  let translateY = $(document).height() * 0.5;
+  let i = 0;
 
-	// Create an SVG group for a node.
-	// Note: By default, coordinates are for vertical presentation.
-	//       For horizontal presentation, x-y corrdinates are reversed.
-	const nodeEnter = node.enter().append('g')
-											.attr('transform', (d, index) => {
-												translateY += index * 10;
-												return 'translate(' + d.y + ', ' + d.x + ')'
-											});
+  const node = svgGroup.selectAll('g.nodes')
+                .data(nodes, (item) => item.id || (item.id = i++));
 
-	// Create the circular node in the group.
-	nodeEnter.append('circle')
-		.attr('class', 'node-circle')
-		.attr('r', 10)
-		.style("fill", function(d) {
-	    return d._children || d.children ? "#0f0" : "#f00";
-	  });
+  // Create an SVG group for a node.
+  // Note: By default, coordinates are for vertical presentation.
+  //       For horizontal presentation, x-y corrdinates are reversed.
+  const nodeEnter = node.enter().append('g')
+                      .call(dragListener)
+                      .attr('transform', (d, index) => {
+                        translateY += index * 10;
+                        return 'translate(' + d.y + ', ' + d.x + ')'
+                      });
 
-	// Create text for the node.
-	nodeEnter.append('text')
-		.text((item) => item.name);
+  // Create the circular node in the group.
+  nodeEnter.append('circle')
+    .attr('class', 'node-circle')
+    .attr('r', 10)
+    .style("fill", function(d) {
+      return d._children || d.children ? "#0f0" : "#f00";
+    });
+
+  // Create text for the node.
+  nodeEnter.append('text')
+    .text((item) => item.name);
 
 
   // Create links path.
-	const link = svgGroup.selectAll("path.link")
-					 .data(links, function(d) {
-							 return d.target.id;
-					 });
+  const link = svgGroup.selectAll("path.link")
+           .data(links, function(d) {
+               return d.target.id;
+           });
 
-	link.enter()
-		.insert('path', 'g')
-		.attr('fill', 'none')
-		.attr('stroke', 'blue')
-		.attr('class', 'link')
-		.attr('d', diagonal);
+  link.enter()
+    .insert('path', 'g')
+    .attr('fill', 'none')
+    .attr('stroke', 'blue')
+    .attr('class', 'link')
+    .attr('d', diagonal);
 
 link.exit().remove();
 
 }catch (e) {
-	debugger
+  debugger
 }
 });
